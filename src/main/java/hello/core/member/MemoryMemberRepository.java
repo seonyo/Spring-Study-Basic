3149a482b279f2ce97d000cbf5b556087a335ca0
seonyo <jsy6535@naver.com>
2024-02-05 20:26:20 +0900

    ✨ MemberService 구현체 생성

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -3,7 +3,7 @@ package hello.core.member;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MemberRepository implements MemoryRepository{
+public class MemoryMemberRepository implements MemoryRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
     @Override
