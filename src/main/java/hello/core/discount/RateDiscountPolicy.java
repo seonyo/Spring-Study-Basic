f84452ca187e7b41a77c88d97daf7c0f3164c5d2
seonyo <jsy6535@naver.com>
2024-02-15 03:58:17 +0900

    ✨의존관계 자동 주입하기

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,7 +2,9 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 public class RateDiscountPolicy implements DiscountPolicy{
     private int discountPercent = 10;
     @Override
